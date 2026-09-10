58d814bddf365452e9cdb1e8105b752d91802d34
mannysan57 <mannysan@iastate.edu>
2026-09-10 14:42:54 -0500

    Update navigation link from Welcome to Education

--- navigation.js
+++ navigation.js
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
       </li>
 
       <li>
-        <a href="https://mannysan57.github.io/Manny-Sandoval-Experience/welcome.html">
-          Welcome
+        <a href="https://mannysan57.github.io/Manny-Sandoval-Experience/education.html">
+          Education
         </a>
       </li>
 
